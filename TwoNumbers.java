5963f66b8da79144b56b6306e4f301379a0dd819
SomaSekhar9112 <150917513+SomaSekhar9112@users.noreply.github.com>
2023-12-17 12:32:48 +0530

    Added two numbers function

---
+++ TwoNumbers.java
@@ -0,0 +1,32 @@
+package learnings;
+
+class Twonumbers
+{
+	public static void main(String args[])
+	{
+		int num1 = 25;
+		int num2 = 45;
+		
+		if(num1==num2){
+			
+			System.out.println("the numbers are equal");
+		}
+		
+		else{ 
+			
+		    System.out.println("the numbers are not equal");
+		}
+	
+	    int num3 = 99;
+		int num4 = 99;
+		
+		if(num3==num4){
+			System.out.println("the numbers are equal");
+		}
+		else{
+			System.out.println("the numbers are not equal");
+		}
+		
+	
+	}
+}
